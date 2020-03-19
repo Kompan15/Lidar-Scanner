8f679afa0357b119ed1b7da7e3304a09cb20ef87
RafałM <45499564+Kompan15@users.noreply.github.com>
2020-03-19 10:33:04 +0100

    Update LidarPy.py

--- LidarPy/LidarPy.py
+++ LidarPy/LidarPy.py
@@ -7,24 +7,24 @@ ser = serial.Serial('/dev/ttyAMA0', 115200)
 PwmPin1 = 12
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD) #Sposób numerowania Pinów
-GPIO.setup(PwmPin1,GPIO.OUT) #SetMode
-pi_pwm = GPIO.PWM(PwmPin1,1000) #czestotliwosc
-pi_pwm.start(80) #Stopien Wypelnienia
+GPIO.setup(PwmPin1,GPIO.OUT) #Definicja Pinu
+pi_pwm = GPIO.PWM(PwmPin1,1000) #GPIO.PWM(channel, frequency)
+pi_pwm.start(80) #%Wypełnienia sygnału w trakcie jednego okresu.
 
 GPIO.setup(PwmPin1, GPIO.OUT)
 
-def getTFminiData():
+def getTFminiData(): #Definicja Funkcji.
     millis1 = int(round(time.time() * 1000)) #klasyk, zbierz czas do roznicy
     while True:
         count = ser.in_waiting
         if count > 8:
             recv = ser.read(9)
             ser.reset_input_buffer()
-            if recv[0] == 'Y' and recv[1] == 'Y': # 0x59s to 'Y', bez interpretacji terminal wypluwa same YYYY
+            if recv[0] == 'Y' and recv[1] == 'Y': # 0x59s to 'Y', To są pierwsze dwa bajty info.
                 low = int(recv[2].encode('hex'), 16)
                 high = int(recv[3].encode('hex'), 16)
                 distance = low + high * 256
-                millis = int(round(time.time() * 1000))
+                millis = int(round(time.time() * 1000)) #Zbierz czas na zakonczenie funkcji.
                 print(millis-millis1), #przecinek pozwala na printline.
                 print(distance)
                 
